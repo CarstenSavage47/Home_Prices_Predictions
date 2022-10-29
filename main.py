9a1b366ef6398d3d2f0449a0792a41f068ea86fe
CarstenSavage47 <rampagev2@gmail.com>
2022-10-28 21:07:06 -0700

    Adding linear regression

--- main.py
+++ main.py
@@ -1,14 +1,138 @@
 # Thank you to Christian Versloot for help with implementing a linear regression in PyTorch.
-# https://github.com/christianversloot/machine-learning-articles/blob/main/how-to-create-a-neural-network-for-regression-with-pytorch.md
+# https://github.com/christianversloot/machine-learning-articles/blob/main/how-to-create-a-neural-
+# network-for-regression-with-pytorch.md
 
 import pandas
+import numpy as np
 import openpyxl
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
-from sklearn.datasets import load_boston
 from sklearn.preprocessing import StandardScaler
+import torch  # torch provides basic functions, from setting a random seed (for reproducability) to creating tensors.
+import torch.nn.functional as F  # nn.functional give us access to the activation and loss functions.
+from torch.optim import SGD  # optim contains many optimizers. Here, we're using SGD, stochastic gradient descent.
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn import preprocessing
+from statsmodels.formula.api import ols
+import matplotlib.pyplot as plt  ## matplotlib allows us to draw graphs.
+import seaborn as sns  ## seaborn makes it easier to draw nice-looking graphs.
+import os
+from tqdm import tqdm
+from pylab import rcParams
+import matplotlib.pyplot as plt
+from matplotlib import rc
+import plotly.express as px
+
 
 HomePrices = pandas.read_csv('/Users/carstenjuliansavage/PycharmProjects/RE_Regression/House Prices.csv')
 
-## To be continued...
+# Group columns by datatypes for inspection
+Column_VarType_Dict = HomePrices.columns.to_series().groupby(HomePrices.dtypes).groups
+Column_VarType_Dict
+
+HomePrices = pandas.get_dummies(HomePrices)
+
+X = np.array(HomePrices.drop(['SalePrice'],axis=1))
+y = np.array(HomePrices['SalePrice'])
+
+class RE_Dataset(torch.utils.data.Dataset):
+    '''
+    Prepare the dataset for regression
+    '''
+
+    def __init__(self, X, y, scale_data=True):
+        if not torch.is_tensor(X) and not torch.is_tensor(y):
+            # Apply scaling if necessary
+            if scale_data:
+                X = StandardScaler().fit_transform(X)
+            self.X = torch.from_numpy(X)
+            self.y = torch.from_numpy(y)
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, i):
+        return self.X[i], self.y[i]
+
+
+class MLP(nn.Module):
+    '''
+      Multilayer Perceptron for regression.
+    '''
+
+    def __init__(self):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(289, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, 1)
+        )
+
+    def forward(self, x):
+        '''
+          Forward pass
+        '''
+        return self.layers(x)
+
+
+if __name__ == '__main__':
+
+    # Set fixed random number seed
+    torch.manual_seed(47)
+
+    # Prepare Boston dataset
+    dataset = RE_Dataset(X, y)
+    trainloader = torch.utils.data.DataLoader(dataset, batch_size=10, shuffle=True, num_workers=0)
+
+    # Initialize the MLP
+    mlp = MLP()
+
+    # Define the loss function and optimizer
+    loss_function = nn.L1Loss()
+    optimizer = torch.optim.Adam(mlp.parameters(), lr=1e-4)
+
+    # Run the training loop
+    for epoch in range(0, 5):  # 5 epochs at maximum
+
+        # Print epoch
+        print(f'Starting epoch {epoch + 1}')
+
+        # Set current loss value
+        current_loss = 0.0
+
+        # Iterate over the DataLoader for training data
+        for i, data in enumerate(trainloader, 0):
+
+            # Get and prepare inputs
+            inputs, targets = data
+            inputs, targets = inputs.float(), targets.float()
+            targets = targets.reshape((targets.shape[0], 1))
+
+            # Zero the gradients
+            optimizer.zero_grad()
+
+            # Perform forward pass
+            outputs = mlp(inputs)
+
+            # Compute loss
+            loss = loss_function(outputs, targets)
+
+            # Perform backward pass
+            loss.backward()
+
+            # Perform optimization
+            optimizer.step()
+
+            # Print statistics
+            current_loss += loss.item()
+            if i % 10 == 0:
+                print('Loss after mini-batch %5d: %.3f' %
+                      (i + 1, current_loss / 500))
+                current_loss = 0.0
+
+    # Process is complete.
+    print('Training process has finished.')
